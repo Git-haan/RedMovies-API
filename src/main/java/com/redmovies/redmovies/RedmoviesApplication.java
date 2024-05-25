cdd726c308013c88e232e447a5a2fdad7ca825e3
Git-haan <ishaan.reddy5@gmail.com>
2024-05-25 18:39:32 -0400

    api

---
+++ src/main/java/com/redmovies/redmovies/RedmoviesApplication.java
@@ -0,0 +1,14 @@
+package com.redmovies.redmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RedmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redmovies/redmovies/RedmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmovies.redmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
